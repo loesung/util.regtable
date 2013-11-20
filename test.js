01e47bef5ec7ea9b9f85704c40379470a36d9e0d
NeoAtlantis <rijndael@yeah.net>
2013-11-21 02:39:59 +0800

    save

--- test.js
+++ test.js
@@ -5,9 +5,10 @@ var socketPath = CONFIG.get('socket-path');
 var IPCClient = $.net.IPC.client(socketPath);
 
 IPCClient.request(
-    '/',    
+    '/abc/def',    
     function(err, e){
         if(null == err){
+            console.log(e.response.statusCode);
             console.log(e.response.headers);
             e.on('ready', function(data){
                 console.log(data);
